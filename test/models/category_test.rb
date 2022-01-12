9525835620fea7144d1c01af36d4591409ea78f5
Zhenya Telyukov <telyukov@gmail.com>
2022-01-12 18:19:03 +0300

    category test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @category = Category.new
+  end
+
+  context 'validations' do
+    should validate_presence_of(:name)
+    should validate_uniqueness_of(:name)
+    should validate_presence_of(:title)
+    should validate_uniqueness_of(:title)
+    should validate_presence_of(:url)
+    should validate_uniqueness_of(:url)
+    should validate_presence_of(:seo_title)
+  end
 end
